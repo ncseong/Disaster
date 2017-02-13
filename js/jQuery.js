ae331cb5657fdf967addcc0b08aa20ffc75d0dbe
chyoon1217 <chyoon@sphinfo.co.kr>
2017-02-13 11:23:51 +0900

    마우스 hover시 이미지 변경 소스 수정

--- js/jQuery.js
+++ js/jQuery.js
@@ -2,20 +2,10 @@ $(document).ready(function() {
   $('#tool').hide();
 
   // 마우스오버시 이미지 변경
-  $('#area').hover(function() {
-    $(this).children("img").attr("src","images/area_on.gif");
+  $('#top-bar').find('img').hover(function() {
+    $(this).attr("src",$(this).attr("src").replace(/off\.gif$/, 'on.gif'));
   }, function() {
-    $(this).children("img").attr("src","images/area_off.gif");
-  });
-  $('#info').hover(function() {
-    $(this).children("img").attr("src","images/info_on.gif");
-  }, function() {
-    $(this).children("img").attr("src","images/info_off.gif");
-  });
-  $('#btn_tool').hover(function() {
-    $(this).children("img").attr("src","images/btn_tool_on.gif");
-  }, function() {
-    $(this).children("img").attr("src","images/btn_tool_off.gif");
+    $(this).attr("src",$(this).attr("src").replace(/on\.gif$/, 'off.gif'));
   });
 
   // 정보표시 클릭시 TOC 토글
